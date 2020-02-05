4494495605c31c0ba5bd53b7c6a79461cdfbd720
Zarina Ali <z.ali2269@student.leedsbeckett.ac.uk>
2020-02-05 20:51:38 +0000

    Revert "CSS not working"
    This reverts commit a23486fda783e9d0209957943fce14dfd0f90835.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,95 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('title', 'Welcome')
+        <title>Laravel</title>
 
-@section('content')
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading" style="text-align:center;background-color:hsla(168, 55%, 94%)">
-                <h2 style="color:hsla(168, 55%, 65%)">Welcome</h2></div>
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
 
-                <div class="panel-body">
-                    <ul>
-                    @foreach($data as $data)
-                    <li>{{ $data }}</li>
-                    @endforeach
-                    </ul> 
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
                 </div>
-             </div>
-          </div>
-      </div>
-</div>
-@endsection
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,16 @@
 <?php
-/* EPISODE 22:)
- * you can create own service providers and packages laravel, I didnt!
- * Laravel is constructed through a tonne of components which all contain a service
- * provider that knows how to register and bootstrap itself with the framework
- */
-
-/* EPISODE 23:)
- * Config isn't really for development as it is an performance optimization
- */
-
-Route::get('/', 'PagesController@home');
-
-Route::get('/pink', 'PagesController@pink');
-Route::get('/blue', 'PagesController@blue');
-
 
 /*
- * GET /projects (index)
- * GET /projects/create (create)
- * GET /projects/1 (show)
- * POST /projects (store)
- * GET /projects/1/edit (edit)
- * PATCH /projects/1 (update)
- * DELETE /projects/1 (destroy)
- */
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::resource('blogs', 'BlogsController');
-
-Route::post('/blogs/{blog}/tasks', 'BlogTasksController@store');
-Route::patch('/tasks/{task}', 'BlogTasksController@update');
-
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
